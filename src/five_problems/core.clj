f0aab7770f0d2c3a050e3699c1ded41c4021b6f4
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:01:48 -0400

    formatting of results

--- src/five_problems/core.clj
+++ src/five_problems/core.clj
@@ -1,16 +1,28 @@
 (ns five-problems.core
+  (:gen-class)
   (:use [five-problems.problem-1 :only [problem-1]]
         [five-problems.problem-2 :only [problem-2]]
         [five-problems.problem-3 :only [problem-3]]
         [five-problems.problem-4 :only [problem-4]]
         [five-problems.problem-5 :only [problem-5]]))
 
-(defn -main
+(defn -main 
   "Run each of the problems from here"  
-  []
+  [& args]
   (do
+    (println "problem-1")
     (problem-1)
-    (problem-2)
-    (problem-3)
+    (println)
+    (println "problem-2")
+    (println (problem-2))
+    (println)
+    (println "problem-3")
+    (println (problem-3))
+    (println)
+    (println "problem-4")
     (problem-4)
-    (problem-5)))
+    (println)
+    (println "problem-5")
+    (println (problem-5))
+    (println)
+))
