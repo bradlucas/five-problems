7a61253dd99eb84629a584fa53af73f3603ae301
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:01:25 -0400

    formatting

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.3"]]
-  :main five-problems.core)
+
+  :main five-problems.core
+)
