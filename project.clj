8dfe75295ed12d0cf78e75ae05954f3faa2d4173
Brad Lucas <brad@beaconhill.com>
2016-10-15 21:53:35 -0400

    latest clojure 1.8.0 and math.combinatorics 0.1.3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject five-problems "0.1.0-SNAPSHOT"
+(defproject five-problems "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.3"]]
   :main five-problems.core)
